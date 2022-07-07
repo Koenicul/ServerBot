8d7e1510f9a4e18d70fd71d361eb21b101781e99
Koenicul <koenvaartjes2005@gmail.com>
2022-07-07 16:05:18 +0200

    Owner check

--- cogs/server.py
+++ cogs/server.py
@@ -3,6 +3,7 @@ import discord
 import db
 from sqlalchemy.orm import sessionmaker
 import docker
+import os
 
 Session = sessionmaker(bind=db.engine)
 session = Session()
@@ -15,20 +16,23 @@ class Server(commands.Cog):
     
     @commands.command(name="Start", help="Start a docker container")
     async def Start(self, ctx, container):
-        con = client.containers.get(container)
-        con.start()
-        await ctx.send(f"{con.name} started")
+        if ctx.author.id == os.getenv("Owner_id"):
+            con = client.containers.get(container)
+            con.start()
+            await ctx.send(f"{con.name} started")
 
     @commands.command(name="Stop", help="Stop a docker container")
     async def Stop(self, ctx, container):
-        con = client.containers.get(container)
-        con.stop()
-        await ctx.send(f"{con.name} stopped")
+        if ctx.author.id == os.getenv("Owner_id"):
+            con = client.containers.get(container)
+            con.stop()
+            await ctx.send(f"{con.name} stopped")
 
     @commands.command(name="ListContainers", help="List all containers")
     async def ListContainers(self, ctx):
-        for i in client.containers.list(all=True):
-            await ctx.send(f"{i.name}")
+        if ctx.author.id == os.getenv("Owner_id"):
+            for i in client.containers.list(all=True):
+                await ctx.send(f"{i.name}")
 
 def setup(bot):
     bot.add_cog(Server(bot))
