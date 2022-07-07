682ffd2fffe2550d277127da5bd8de73f0b93688
Koenicul <koenvaartjes2005@gmail.com>
2022-07-07 15:45:07 +0200

    Get db_url from .env

--- db.py
+++ db.py
@@ -1,10 +1,13 @@
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///data.sqlite', echo=False)
+engine = create_engine(os.getenv("db_url"), echo=False)
 
 class Server(Base):
     __tablename__ = "server"
